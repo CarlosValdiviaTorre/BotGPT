782a104b2e37b92cabd7bc3ee2e638f64572fc72
CarlosValdiviaTorre <carlos.valdivia@bombeatec.com>
2023-11-18 12:01:16 +0100

    servertest

--- src/server.js
+++ src/server.js
@@ -1,27 +1,31 @@
-require('dotenv').config();
-const express = require('express');
-const OpenAI = require('openai');
-const bodyParser = require('body-parser');
+import 'dotenv/config'; // Reemplaza require('dotenv').config();
+import express from 'express';
+import { OpenAI } from 'openai'; // Nota el uso de destructuring aquí
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Utiliza la clave API desde las variables de entorno
-const openai = new OpenAI(process.env.OPENAI_API_KEY);
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-app.use(bodyParser.json());
+// bodyParser.json() es parte de express a partir de la versión 4.16+
+app.use(express.json());
 
 app.post('/ask', async (req, res) => {
     try {
         const { message } = req.body;
 
+        // Cambios en la API pueden requerir que ajustes esta parte
         const completion = await openai.createCompletion({
             model: "gpt-4",
             prompt: message,
             max_tokens: 150
         });
 
-        res.json({ response: compwwwletion.choices[0].message.content });
+        res.json({ response: completion.choices[0].message.content });
     } catch (error) {
         console.error(error);
         res.status(500).send('Ocurrió un error en el servidor');
