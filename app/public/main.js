2e4375adbe476dd9810edec3a8698c403c0099d1
CarlosValdiviaTorre <carlos.valdivia@bombeatec.com>
2023-11-18 11:06:39 +0100

    back v1

--- app/public/main.js
+++ app/public/main.js
@@ -1,2 +1,61 @@
-// JavaScript file
-console.log("Hola que tal, soy un bot de GPT para realizar consultas a bases de datos");
+document.addEventListener('DOMContentLoaded', () => {
+    const sendButton = document.getElementById('send-button');
+    const userInput = document.getElementById('user-input');
+    const messageArea = document.getElementById('message-area');
+
+    // Función para enviar el mensaje y recibir la respuesta
+    async function sendMessage(message) {
+        try {
+            // Aquí enviarías el mensaje a tu API
+            const response = await fetch('TU_ENDPOINT_DE_API', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // Aquí irían otros headers necesarios, como tokens de autenticación
+                },
+                body: JSON.stringify({ message: message })
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            // Asumiendo que 'data' contiene la respuesta del chatbot
+            displayMessage(data.reply, 'bot');
+        } catch (error) {
+            console.error('Error al enviar el mensaje:', error);
+        }
+    }
+
+    // Función para mostrar el mensaje en la pantalla
+    function displayMessage(message, sender) {
+        const messageDiv = document.createElement('div');
+        messageDiv.classList.add('message', sender);
+        messageDiv.textContent = message;
+
+        messageArea.appendChild(messageDiv);
+
+        // Desplaza el área de mensajes al último mensaje
+        messageArea.scrollTop = messageArea.scrollHeight;
+    }
+
+    sendButton.addEventListener('click', () => {
+        const message = userInput.value.trim();
+
+        if (message) {
+            displayMessage(message, 'user');
+            sendMessage(message);
+            userInput.value = ''; // Limpia el campo de entrada
+        }
+    });
+
+    // Opcional: enviar mensaje al presionar 'Enter'
+    userInput.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            sendButton.click();
+        }
+    });
+});
