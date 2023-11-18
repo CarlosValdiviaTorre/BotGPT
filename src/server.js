3ce997c8e0d628eec1838549b59a591ea9b0ac53
CarlosValdiviaTorre <carlos.valdivia@bombeatec.com>
2023-11-18 11:38:49 +0100

    envTest1

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const OpenAI = require('openai');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Asegúrate de reemplazar esto con tu clave de API real y mantenerla protegida.
-const openai = new OpenAI('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');
+// Utiliza la clave API desde las variables de entorno
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
 app.use(bodyParser.json());
 
@@ -20,7 +21,7 @@ app.post('/ask', async (req, res) => {
             max_tokens: 150
         });
 
-        res.json({ response: completion.choices[0].message.content });
+        res.json({ response: compwwwletion.choices[0].message.content });
     } catch (error) {
         console.error(error);
         res.status(500).send('Ocurrió un error en el servidor');
